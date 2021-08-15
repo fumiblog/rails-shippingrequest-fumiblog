6f4e66ae2fba8ac338125a1791bc004da186d660
fumiblog <71933858+fumiblog@users.noreply.github.com>
2021-08-15 19:56:25 +0900

    fix js_compressor2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  # config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
 end
