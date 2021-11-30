74b48388214b18b2f53207f92340c29b833f5286
fumiblog <71933858+fumiblog@users.noreply.github.com>
2021-11-30 22:03:07 +0900

    [fullcalendar2]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: '出荷ソフト',
+      title: 'トップページ',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ご訪問ありがとうございます。出荷ソフトをruby on ralesで作成しました。',
+      keywords: 'rails,ruby,プログラミング,bike,coffee',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('kashiwagi_logo.png') }
+      ],
+      og: {
+        site_name: :site, # もしくは site_name: :site
+        title: :title, # もしくは title: :title
+        description: :description, # もしくは description: :description
+        type: 'shipping',
+        url: request.original_url,
+        image: image_url('kashiwagi_logo.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@fumihisa_y',
+      }
+    }
+  end
 end
