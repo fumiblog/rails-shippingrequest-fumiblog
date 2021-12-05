f3b55ccbe0cb2a227439d554d611d2c9b666b463
fumiblog <71933858+fumiblog@users.noreply.github.com>
2021-12-05 20:26:59 +0900

    [index3]

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -5,7 +5,8 @@ class Users::HomesController < ApplicationController
   def index
     @heads = Head.all
     # @day_heads = Head.all
-    @day_heads = Head.where(ship_date: "2021-12-05 17:50:00.000000000")
+    @day_heads = Head.where(ship_date: Time.current.in_time_zone.all_day)
+    # @day_heads = Head.where(ship_date: "2021-12-05 17:50:00.000000000")
     if user_signed_in?
       # gon.user_name = Head.where(ship_date: cure)
       gon.user_name = current_user.name
