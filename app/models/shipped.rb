de71caab4d27386b8422a62280d8a32a05e9e99a
fumiblog <71933858+fumiblog@users.noreply.github.com>
2022-01-13 04:22:58 +0900

    [person5]

--- app/models/shipped.rb
+++ app/models/shipped.rb
@@ -1,12 +1,4 @@
 class Shipped < ApplicationRecord
   belongs_to :head
   belongs_to :body
-  enum unic: {
-    none: false,
-    yes: true 
-  }
-  enum waiting_place: {
-    none: false,
-    yes: true
-  }
 end
