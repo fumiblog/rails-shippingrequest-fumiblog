70002790affa5dc9e2f2380dd6e1492a2c7b0168
fumiblog <71933858+fumiblog@users.noreply.github.com>
2022-01-22 21:38:36 +0900

    [pdf2]

--- app/pdfs/record_pdf.rb
+++ app/pdfs/record_pdf.rb
@@ -18,7 +18,22 @@ class RecordPdf < Prawn::Document
   end
 
   def content
-    text 'hello こんにちは 春夏秋冬'
+    text '矢野　文久（ヤノ　フミヒサ）
+
+    株式会社 柏木興産　県南営業部
+
+    〒830-0044 久留米市本町10-26
+
+    丸善ビル2F
+
+    TEL:0942-33-1456 
+    
+    FAX:0942-33-1459
+
+    email:fyano@kashiwagi-k.co.jp
+    
+    Handy Phone:090-5565-1801'
+    # text 'hello こんにちは 春夏秋冬'
   end
 
 end
