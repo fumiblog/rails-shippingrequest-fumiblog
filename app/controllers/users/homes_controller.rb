6eacad913671a8fbb779177aaa511c1ede536131
fumiblog <71933858+fumiblog@users.noreply.github.com>
2022-01-29 04:51:35 +0900

    [home21]

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -11,9 +11,11 @@ class Users::HomesController < ApplicationController
       @cs = Company.where(user_id: current_user.id)
       @ps = Person.where(company_id: @cs.ids)
       @jobs = Job.where(person_id: @ps.ids)
+      @job_periods = @jobs.where(date: Date.today - 7 .. Date.today + 10)
       @user_heads = Head.where(user_id: current_user.id)
       # byebug
-      @day_heads = @user_heads.where(ship_date: Time.current.in_time_zone.all_day)
+      @day_heads = @user_heads.where(ship_date: Time.current.in_time_zone.all_week)
+      # @day_heads = @user_heads.where(ship_date: Time.current.in_time_zone.all_day)
     else
       @day_heads = Head.where(ship_date: Time.current.in_time_zone.all_day)
     end
