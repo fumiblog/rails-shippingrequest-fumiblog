22a67f234c0ff5dbe23ea72630f67c4517eefc74
fumiblog <71933858+fumiblog@users.noreply.github.com>
2022-02-05 11:12:13 +0900

    [home53]

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -11,7 +11,9 @@ class Users::HomesController < ApplicationController
       @cs = Company.where(user_id: current_user.id)
       @ps = Person.where(company_id: @cs.ids)
       @jobs = Job.where(person_id: @ps.ids)
-      @job_periods = @jobs.where(date: Date.today - 7 .. Date.today + 10).order(date: "DESC")
+      @job_incompletes = @jobs.where(completed: "false")
+      @job_pasts = @job_incompletes.where("date <= ?", Date.today - 7 )
+      @job_periods = @job_pasts + @jobs.where(date: Date.today - 7 .. Date.today + 10).order(date: "DESC")
       @user_heads = Head.where(user_id: current_user.id)
       # byebug
       @day_heads = @user_heads.where(ship_date: Time.new - 1.day .. Time.new + 1.week)
