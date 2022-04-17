37d55f6aec20cf7e3c352ab6aef92f975a208034
fumiblog <71933858+fumiblog@users.noreply.github.com>
2022-04-17 09:54:35 +0900

    pdf21

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :customers, skip: :all
+  devise_scope :customer do
+    get 'customers/sign_in' => 'customers/sessions#new', as: 'new_customer_session'
+    post 'customers/sign_in' => 'customers/sessions#create', as: 'customer_session'
+    delete 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customer_session'
+    get 'customers/sign_up' => 'customers/registrations#new', as: 'new_customer_registration'
+    post 'customers' => 'customers/registrations#create', as: 'customer_registration'
+    get 'customers/password/new' => 'customers/passwords#new', as: 'new_customer_password'
+  end
+  
+
   devise_for :admins
+
   devise_for :users, skip: :all
   devise_scope :user do
     get 'users/sign_in' => 'users/sessions#new', as: 'new_user_session'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_12_083254) do
+ActiveRecord::Schema.define(version: 2022_04_10_045618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,18 @@ ActiveRecord::Schema.define(version: 2022_03_12_083254) do
     t.integer "company_id"
     t.string "image"
     t.boolean "complete"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "form_bases", force: :cascade do |t|
